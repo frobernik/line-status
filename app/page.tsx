a0649bd44b7f0976bc8d5162cf06b50a418543d5
jhbaker <jhbaker@runbox.com>
2024-05-06 17:20:02 +0100

    Added disruption tyling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import { fetchAPI } from '@/lib/fetchAPI';
 
-// const GOOD_SERVICE = 'Good Service';
+const GOOD_SERVICE = 'Good Service';
+
+type ValidityPeriods = {
+    $type: string;
+    category: string;
+    categoryDescription: string;
+    description: string;
+    created: string;
+    affectedRoutes: string[];
+    affectedStops: string[];
+    closureText: string;
+};
 
 type LineStatus = {
     $type: string;
@@ -26,26 +37,16 @@ type TubeLineStatusType = {
     lineStatuses: LineStatus[];
 };
 
-type ValidityPeriods = {
-    $type: string;
-    category: string;
-    categoryDescription: string;
-    description: string;
-    created: string;
-    affectedRoutes: string[];
-    affectedStops: string[];
-    closureText: string;
-};
-
 function TubeLineName({ id, name }: TubeLineNameType) {
-    return (<div className={`w-2/3 line line--${id}`}>
+    return (<div className={`p-2.5 w-1/2 line line--${id}`}>
         <dt className="sr-only" aria-hidden="true">Name</dt>
         <dd id={`title--${id}`}>{name}</dd>
     </div>);
 }
 
 function TubeLineStatus({lineStatuses}: TubeLineStatusType) {
-    return (<div className="w-1/3">
+    const hasDisruption = lineStatuses.filter(({ statusSeverityDescription }) => statusSeverityDescription !== GOOD_SERVICE);
+    return (<div className={`p-2.5 w-1/2${hasDisruption.length > 0 ? ' line--disruption' : ''}`}>
         <dt className="sr-only" aria-hidden="true">Status</dt>
         {lineStatuses.map(({statusSeverityDescription}) => <dd>{statusSeverityDescription}</dd>)}
     </div>);
@@ -53,7 +54,7 @@ function TubeLineStatus({lineStatuses}: TubeLineStatusType) {
 
 function TubeLine({id, lineStatuses, name }: TubeLineType) {
     return (
-        <dl key={id} className="border border-gray-300 p-2.5 pl-5 flex justify-between relative sm:columns-2 gap-x-0 break-inside-avoid-column" role="heading">
+        <dl key={id} className="border border-gray-300 pl-2.5 flex justify-between relative sm:columns-2 gap-x-0 break-inside-avoid-column" role="heading">
             <TubeLineName id={id} name={name} />
             <TubeLineStatus lineStatuses={lineStatuses} />
         </dl>
